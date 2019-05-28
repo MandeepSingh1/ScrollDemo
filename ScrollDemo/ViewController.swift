c93ae00786660007a7ca51648164383e6dedafb7
Mandeep Singh <mandeep.singh671@gmail.com>
2019-05-28 12:00:25 +0530

    code commit by mandeep singh

--- ScrollDemo/ViewController.swift
+++ ScrollDemo/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView : UIScrollView!
+    @IBOutlet weak var view1 : UIView!
+    @IBOutlet weak var view2 : UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func notification(_ sender: Any) {
+        UIView.animate(withDuration: 0.25) {
+            let rect = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+            self.scrollView.scrollRectToVisible(rect, animated: true)
+        }
+    }
+    
+    @IBAction func invitation(_ sender: Any) {
+        UIView.animate(withDuration: 0.25) {
+            let x = self.view.frame.width
+            let rect = CGRect(x: x, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+            self.scrollView.scrollRectToVisible(rect, animated: true)
+        }
+    }
+    
 }
 
